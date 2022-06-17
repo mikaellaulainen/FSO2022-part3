0c50f529ae0f8a694e7a9680e543dc52324123f8
mikaellaulainen <laulainenmikael@gmail.com>
2022-06-17 12:42:34 +0300

    3.15-3.16 and 3.18

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.get('/api/persons', (request, response) => {
 //add person
 app.post('/api/persons', (request,response) => {
   const body = request.body
+
   if (!body.name){
     return response.status(400).json({
       error: 'Name missing'
@@ -42,31 +43,60 @@ app.post('/api/persons', (request,response) => {
 
 //Date info and amount of persons
 app.get('/info', (req,res) => {
-    const dateInfo= new Date()
-    res.send(`
-        <p>Phonebook has info for ${persons.length} people</p>
-        <p>${dateInfo}</p>
-    
-    `)
+  const dateInfo= new Date()
+  Person.countDocuments(function(err,count){
+    if (err){
+      console.log(err)
+    } else{
+      res.send(`
+      <p>Phonebook has info for ${count} people</p>
+      <p>${dateInfo}</p>
+  
+      `)
+    }
+  })
 })
 // get person by id 
 app.get('/api/persons/:id', (request,response) =>{
-    const id= Number(request.params.id)
-
-    const person = persons.find(person => person.id === id)
-    if (person) {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
         response.json(person)
-    }else{
+      } else {
         response.status(404).end()
-    }
+      }
+    })
+    .catch(error => {
+      next(error)
+    })
 })
 //remove person
 app.delete('/api/persons/:id',(request,response) =>{
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 
